cbeb18ed370b3f0a4422e5f10760dc2ddfd6a3f0
lizy <104609666+cscsnus@users.noreply.github.com>
2022-07-13 00:27:56 +0800

    Update OffByOne.java

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -8,20 +8,6 @@ import static org.junit.Assert.assertTrue;
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        return x == y;
-    }
-    @Test
-    public void TestOffByOne(){
-        OffByOne aa = new OffByOne();
-        assertFalse(aa.equalChars('a','b'));
-        assertTrue((aa.equalChars('a','a')));
-
-    }
-
-    public static void main(String[] args) {
-        char x = 'a';
-        char y = 'A';
-        OffByOne aa = new OffByOne();
-        System.out.println(aa.equalChars(x,y));
+        return (x - y) ==1 || (y - x) ==1;
     }
 }
